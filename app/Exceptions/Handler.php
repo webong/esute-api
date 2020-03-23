88be52cb937ab8862a0d2a010cc73cccf39dd861
wisdomanthoni <wizitendo10@gmail.com>
2020-03-23 20:53:56 +0100

    uprade to laravel seven

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,9 +2,8 @@
 
 namespace App\Exceptions;
 
-use Exception;
-use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Throwable;
 
 class Handler extends ExceptionHandler
 {
@@ -30,10 +29,12 @@ class Handler extends ExceptionHandler
     /**
      * Report or log an exception.
      *
-     * @param  \Exception  $exception
+     * @param  \Throwable  $exception
      * @return void
+     *
+     * @throws \Exception
      */
-    public function report(Exception $exception)
+    public function report(Throwable $exception)
     {
         parent::report($exception);
     }
@@ -42,16 +43,18 @@ class Handler extends ExceptionHandler
      * Render an exception into an HTTP response.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  \Exception  $exception
-     * @return \Illuminate\Http\Response
+     * @param  \Throwable  $exception
+     * @return \Symfony\Component\HttpFoundation\Response
+     *
+     * @throws \Throwable
      */
-    public function render($request, Exception $exception)
+    public function render($request, Throwable $exception)
     {
         if ($request->is('api/*') && $request->ajax() && $exception instanceof ModelNotFoundException) {
             return response()->json([
                 'error' => 'Entry for '.str_replace('App\\', '', $exception->getModel()).' not found'], 404);
         }
-    
+
         return parent::render($request, $exception);
     }
 }
